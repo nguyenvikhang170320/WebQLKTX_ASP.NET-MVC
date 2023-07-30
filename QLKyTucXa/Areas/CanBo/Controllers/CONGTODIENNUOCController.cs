a11aed91cd17aa7537417d570935089b6585a416
TuanKenOfficial <nhatyeuthu2013@gmail.com>
2023-07-31 03:03:44 +0700

    Moi tao trong phan can bo controller CONGTODIENNUOC

---
+++ QLKyTucXa/Areas/CanBo/Controllers/CONGTODIENNUOCController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Model.EF;
+
+namespace QLKyTucXa.Areas.CanBo.Controllers
+{
+    public class CONGTODIENNUOCController : Controller
+    {
+        private QLKyTucXaDbContext db = new QLKyTucXaDbContext();
+
+        // GET: CanBo/CONGTODIENNUOC
+        public ActionResult Index()
+        {
+            var cONGTODIENs = db.CONGTODIENs.Include(c => c.PHONG);
+            return View(cONGTODIENs.ToList());
+        }
+
+        // GET: CanBo/CONGTODIENNUOC/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONGTODIEN cONGTODIEN = db.CONGTODIENs.Find(id);
+            if (cONGTODIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONGTODIEN);
+        }
+
+        // GET: CanBo/CONGTODIENNUOC/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_PHONG = new SelectList(db.PHONGs, "ID_PHONG", "MAPHONG");
+            return View();
+        }
+
+        // POST: CanBo/CONGTODIENNUOC/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_DIEN,ID_PHONG,CHISODAU,CHISOCUOI,THANG,NAM,TRANGTHAI")] CONGTODIEN cONGTODIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CONGTODIENs.Add(cONGTODIEN);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_PHONG = new SelectList(db.PHONGs, "ID_PHONG", "MAPHONG", cONGTODIEN.ID_PHONG);
+            return View(cONGTODIEN);
+        }
+
+        // GET: CanBo/CONGTODIENNUOC/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONGTODIEN cONGTODIEN = db.CONGTODIENs.Find(id);
+            if (cONGTODIEN == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_PHONG = new SelectList(db.PHONGs, "ID_PHONG", "MAPHONG", cONGTODIEN.ID_PHONG);
+            return View(cONGTODIEN);
+        }
+
+        // POST: CanBo/CONGTODIENNUOC/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_DIEN,ID_PHONG,CHISODAU,CHISOCUOI,THANG,NAM,TRANGTHAI")] CONGTODIEN cONGTODIEN)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cONGTODIEN).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_PHONG = new SelectList(db.PHONGs, "ID_PHONG", "MAPHONG", cONGTODIEN.ID_PHONG);
+            return View(cONGTODIEN);
+        }
+
+        // GET: CanBo/CONGTODIENNUOC/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CONGTODIEN cONGTODIEN = db.CONGTODIENs.Find(id);
+            if (cONGTODIEN == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cONGTODIEN);
+        }
+
+        // POST: CanBo/CONGTODIENNUOC/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CONGTODIEN cONGTODIEN = db.CONGTODIENs.Find(id);
+            db.CONGTODIENs.Remove(cONGTODIEN);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
